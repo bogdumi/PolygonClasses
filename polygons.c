20bcf36836bec66a4912c3cd659754ee41cc8da4
Bogdan Dumitrescu <bogdoomy@gmail.com>
2018-10-13 15:39:15 +0100

    Name any polygon

--- polygons.c
+++ polygons.c
@@ -8,9 +8,20 @@
 #include <limits.h>
 #include <assert.h>
 
+const char tens_digit[10][15] = {"null", "null", "icosi", "triaconta", "tetraconta",
+								"pentaconta", "hexaconta", "hexaconta", 
+								"octaconta", "enneaconta"};
+const char ones_digit[10][10] = {"", "hena", "di", "tri", "tetra", "penta", 
+								"hexa", "hepta", "octa", "ennea"};
+
+// Functions used throughout the program ----------------------------
 
 // start() is used as a return to the starting point throughout the program
 void start();
+// getsName() is used throughout the program to return the name of the polygon
+char *getsName(int sides);
+
+// Functions used to convert a string into an integer ---------------
 
 // Check if string only contains digits
 bool digitString(const char str[]){
@@ -31,18 +42,36 @@ long convert(const char length[]) {
     return -1;
 }
 
-void namePolygon(int sides){
+// Auxiliary functions ----------------------------------------------
+
+// Control flow based on number of sides received as input
+void controlFlow(int sides){
 	if (sides == -1){
 		printf("Please enter an integer.\n");
 		start();
+		return;
 	}
 	if (sides < 3){
 		printf("You can't have a polygon with fewer than 3 sides. Try again!\n");
 		start();
+		return;
 	}
 	if (sides > 999){
-
+		printf("That's a really complicated polygon! ");
+		printf("Try a polygon with fewer sides\n");
+		start();
+		return ;
 	}
+	char name[100];
+	strcat(name, getsName(sides));
+
+	if (strchr("aeiou", name[0]))
+		printf ("You're looking at an %s!\n", name);
+	else
+		printf ("You're looking at a %s!\n", name);
+
+	
+	
 }
 
 // Check if all sides are equal
@@ -103,11 +132,87 @@ long triangle(long a, long b, long c) {
 }
 */
 
+// Polygon naming functions -----------------------------------------
+
+// Assign *prefix the names of polygons with a small number of sides (<=20)
+// These polygons have individual names each
+void nameSmallPolygon(int sides, char *prefix){
+	switch (sides){
+		case 3: strcat(prefix, "tri"); break;
+		case 4: strcat(prefix, "tetra"); break;
+		case 5: strcpy(prefix, "penta"); break;
+		case 6: strcpy(prefix, "hexa"); break;
+		case 7: strcpy(prefix, "hepta"); break;
+		case 8: strcpy(prefix, "octa"); break;
+		case 9: strcpy(prefix, "nona"); break;
+		case 10: strcpy(prefix, "deca"); break;
+		case 11: strcpy(prefix, "undeca"); break;
+		case 12: strcpy(prefix, "dodeca"); break;
+		case 13: strcpy(prefix, "triskaideca"); break;
+		case 14: strcpy(prefix, "tetradeca"); break;
+		case 15: strcpy(prefix, "pentadeca"); break;
+		case 16: strcpy(prefix, "hexadeca"); break;
+		case 17: strcpy(prefix, "heptadeca"); break;
+		case 18: strcpy(prefix, "optadeca"); break;
+		case 19: strcpy(prefix, "enneadeca"); break;
+		case 20: strcpy(prefix, "icosa"); break;
+	}
+	return;
+}
+
+// Assign *prefix the names of polygons with a medium number of (20,100) sides
+void nameMediumPolygon(int sides, char *prefix){
+	int sides_tens = sides / 10;
+	int sides_ones = sides % 10;
+
+	strcat(prefix, tens_digit[sides_tens]);
+	if(sides_ones){
+		strcat(prefix, "kai");
+		strcat(prefix, ones_digit[sides_ones]);
+	}
+	return;
+}
+
+// Assign *prefix the names of polygons with a large number of sides [100,999]
+void nameLargePolygon(int sides, char *prefix){
+	int sides_hundreds = sides / 100;
+	int sides_tens = (sides / 10) % 10;
+	int sides_ones = sides % 10;
+
+	if (sides_hundreds != 1){
+		strcat(prefix, ones_digit[sides_hundreds]);
+	}
+	strcat(prefix, "hecta");
+
+	if(sides_tens)
+		strcat(prefix, tens_digit[sides_tens]);
+
+	if(sides_ones){
+		strcat(prefix, "kai");
+		strcat(prefix, ones_digit[sides_ones]);
+	}
+	return;
+}
+
+// Returns the name of polygon based on its sides
+char* getsName(int sides){
+	char name[100]="";
+	if (sides <= 20)
+		nameSmallPolygon(sides, name);
+	else if (sides > 20 && sides < 100)
+		nameMediumPolygon(sides, name);
+	else if (sides <=999)
+		nameLargePolygon(sides, name);
+
+	strcat(name,"gon");
+	return name;
+}
+
 // User interface ---------------------------------------------------
 
 // That's not a polygon! It's a circle!
 void easter(){
-	printf("You might be looking at a circle (or another round object! ");
+	printf("You might be looking at a circle (or another somewhat round object)! ");
 	printf("Stop it, they're not polygons! ");
 	printf("Let's try again.\n");
 	start();
@@ -122,24 +227,20 @@ void start(){
 	char input[100];
 	scanf("%s", input);
 
-	if(!strcmp(input, "infinite")){
+	if (!strcmp(input, "infinite")){
 		easter();
 		return;
 	}
 
 	int sides = convert(input);
-	namePolygon(sides);
+	controlFlow(sides);
 	return;
 }
 
-// Print function for name
-void printName(){
-
-}
-
-
+// Main function ----------------------------------------------------
 int main(int n, char *args[n]){
 	setbuf(stdout, NULL);
+
     if (n == 1) {
         //test();
     }
