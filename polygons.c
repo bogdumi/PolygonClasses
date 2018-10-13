1bbb8eec0fea4d31a0039e940754aff722ee0d7a
Bogdan Dumitrescu <bogdoomy@gmail.com>
2018-10-13 18:54:43 +0100

    Classify triangles and output area and perimeter

--- polygons.c
+++ polygons.c
@@ -7,6 +7,11 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <assert.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+// Arrays of prefixes used for naming polygons ----------------------
 
 const char tens_digit[10][15] = {"null", "null", "icosi", "triaconta", "tetraconta",
 								"pentaconta", "hexaconta", "hexaconta", 
@@ -14,12 +19,20 @@ const char tens_digit[10][15] = {"null", "null", "icosi", "triaconta", "tetracon
 const char ones_digit[10][10] = {"", "hena", "di", "tri", "tetra", "penta", 
 								"hexa", "hepta", "octa", "ennea"};
 
-// Functions used throughout the program ----------------------------
+// User interface functions used throughout the program need prototype
+// ------------------------------------------------------------------
 
 // start() is used as a return to the starting point throughout the program
 void start();
 // getsName() is used throughout the program to return the name of the polygon
 char *getsName(int sides);
+// inputSides is used to ask the user for the lengths' of the polygon
+void inputSides(int sides);
+// Classify a triangle
+void triangle(long a, long b, long c);
+// Classify a quadri
+void quadri(long *sides_length);
+
 
 // Functions used to convert a string into an integer ---------------
 
@@ -47,7 +60,7 @@ long convert(const char length[]) {
 // Control flow based on number of sides received as input
 void controlFlow(int sides){
 	if (sides == -1){
-		printf("Please enter an integer.\n");
+		printf("Please enter an integer. To exit, enter \"stop/exit/(q)uit\".\n");
 		start();
 		return;
 	}
@@ -70,8 +83,8 @@ void controlFlow(int sides){
 	else
 		printf ("You're looking at a %s!\n", name);
 
-	
-	
+	inputSides(sides);
+	return;
 }
 
 // Check if all sides are equal
@@ -83,7 +96,59 @@ bool equalSides(int number_sides, long *sides_length){
 	return 1;
 }
 
+// Calculate the perimeter of a polygon given sides -----------------
+long peri(int sides, long *sides_length){
+	long perimeter = 0;
+	for (int i = 0; i < sides; i++)
+		perimeter += sides_length[i];
+	return perimeter;
+}
+
+double area(int sides, long length){
+	double area, numerator, denominator;
+	numerator = length * length * sides;
+	denominator = 4 * tan (PI / sides);
+	area = numerator / denominator;
+	return area;
+}
+
+// Perform operations on polygon's sides
+void polygonOp(int sides, long *sides_length){
+	char name[30]="";
+	strcat(name, getsName(sides));
+	
+	if (sides == 3){
+		triangle(sides_length[0], sides_length[1], sides_length[2]);
+		return;
+	}
+	if(sides == 4){
+		quadri(sides_length);
+		return;
+	}
+
+	long perimeter = peri(sides, sides_length);
+	printf ("The perimeter of your %s is %ld.\n", name, perimeter);
+
+	if (equalSides(sides, sides_length)){
+		double ar = area(sides, sides_length[0]);
+		printf ("Because it is a regular polygon, ");
+		printf ("the area of your %s is about %0.2lf.\n", name, ar);
+	}
+	start();
+}
+
 // Triangle classify functions --------------------------------------
+
+// Check if 2 sides are equal
+bool isosceles(long a, long b, long c){
+    if (a == b)
+        return 1;
+    if (b == c)
+        return 1;
+    if (c == a)
+        return 1;
+    return 0;
+}
 
 // Check if 2 numbers add up to the third
 bool addUp(long a, long b, long c){
@@ -115,22 +180,56 @@ bool impossibleTriangle(long a, long b, long c){
     return 0;
 }
 
-/*
-// Classify a triangle, given side lengths as longs:
-long triangle(long a, long b, long c) {
-    if (a == b && b == c)
-        return Equilateral;
-    if (isosceles(a,b,c))
-        return Isosceles;
-    if (rightAngle(a,b,c))
-        return Right;
-    if (addUp(a,b,c))
-        return Flat;
-    if (impossibleTriangle(a,b,c))
-        return Impossible;
-    return Scalene;
+// Calculate area given sides of triangle
+double triangleArea(long a, long b, long c){
+	double p = (a + b + c) / 2;
+	double multi = p * (p - a) * (p - b) * (p - c);
+	double area = sqrt (multi);
+	return area;
 }
-*/
+
+// Classify a triangle, given side lengths as longs
+void triangle(long a, long b, long c) {
+	char triangleType[50] = "";
+	double ar = -1;
+
+    if (a == b && b == c)
+        strcat(triangleType, "equilateral");
+    if (isosceles(a,b,c))
+        strcat(triangleType, "isosceles");
+    if (rightAngle(a,b,c))
+        strcat(triangleType, "right");
+    if (addUp(a,b,c))
+        strcat(triangleType, "flat");
+    if (impossibleTriangle(a,b,c)){
+    	printf("These sides can't belong to a triangle! Let's start over.\n");
+    	start();
+    	return;
+    }
+    if (!strcmp(triangleType, ""))
+    	strcat(triangleType, "scalene");
+
+    if (strchr("aeiou",triangleType[0]))
+    	printf("You are looking at an %s trigon (more commonly known as a triangle!).\n", triangleType);
+    else
+    	printf("You are looking at a %s trigon (more commonly known as a triangle!).\n", triangleType);
+
+
+    printf("Its perimeter is %ld,\n", a + b + c);
+    ar = triangleArea(a,b,c);
+    printf("Its area is about %0.2lf.\n", ar);
+    start();
+    return;
+}
+
+// Quadrilateral classify functions ----------------------------------------
+
+
+// Classify a quadri
+void quadri(long *sides_length){
+
+}
+
 
 // Polygon naming functions -----------------------------------------
 
@@ -221,11 +320,15 @@ void easter(){
 
 // Get number of sides
 void start(){
-	printf("Look around yourself! The world is full of polygons. "); 
+	printf("Look around! The world is full of polygons. "); 
 	printf("Look at one and count its sides: ");
 
 	char input[100];
 	scanf("%s", input);
+
+	if (!strcmp(input, "stop") || !strcmp(input, "exit") ||
+		!strcmp(input, "quit") || !strcmp(input, "q"))
+		return;
 
 	if (!strcmp(input, "infinite")){
 		easter();
@@ -235,6 +338,27 @@ void start(){
 	int sides = convert(input);
 	controlFlow(sides);
 	return;
+}
+
+// Ask for sides' lengths
+void inputSides(int sides){
+	printf("Would you also like to input the sides' lengths? (yes/no)\n");
+	char answer[10];
+	scanf("%s", answer);
+	long sides_length[1000];
+	if (!strcmp(answer, "yes") || !strcmp(answer, "y")){
+		printf("Please input the sides' lengths: \n");
+		for (int i = 0; i < sides; i++){
+			scanf("%ld", &sides_length[i]);
+			if(sides_length[i] <= 0){
+				printf("That length can't exist! Let's start over.\n");
+				start();
+			}
+		}
+		polygonOp(sides, sides_length);
+	}
+	else
+		start();
 }
 
 // Main function ----------------------------------------------------
